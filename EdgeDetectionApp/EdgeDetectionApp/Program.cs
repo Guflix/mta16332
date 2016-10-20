6d5d93c1c5d5e17db3b9a757fa1edbd8b8bfacaf
Guflix - Thomas Boelt <thomas@boelthansen.dk>
2016-10-20 13:50:05 +0200

    Threshholding

--- EdgeDetectionApp/EdgeDetectionApp/Program.cs
+++ EdgeDetectionApp/EdgeDetectionApp/Program.cs
@@ -3,15 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.UI;
 
 namespace EdgeDetectionApp
 {
-    class Program
+    class ProgramMain
     {
         static void Main(string[] args)
         {
-            Class1 class1 = new Class1();
-            class1.ConsoleWriter();
+            //Class1 class1 = new Class1();
+            Class2 test = new Class2();
+            test.Threshhold();
+
         }
     }
 }
