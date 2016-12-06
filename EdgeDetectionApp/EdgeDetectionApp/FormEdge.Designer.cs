a1782171510720727bc5e2d3911224b6620fc531
Guflix - Thomas Boelt <thomas@boelthansen.dk>
2016-12-06 13:05:10 +0100

    Størrelser

--- EdgeDetectionApp/EdgeDetectionApp/FormEdge.Designer.cs
+++ EdgeDetectionApp/EdgeDetectionApp/FormEdge.Designer.cs
@@ -39,10 +39,11 @@
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.IndianRed;
+            this.button1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.button1.Location = new System.Drawing.Point(73, 509);
+            this.button1.Location = new System.Drawing.Point(35, 484);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(139, 53);
+            this.button1.Size = new System.Drawing.Size(177, 78);
             this.button1.TabIndex = 1;
             this.button1.Text = "Cirkel";
             this.button1.UseVisualStyleBackColor = false;
@@ -51,9 +52,10 @@
             // button2
             // 
             this.button2.BackColor = System.Drawing.Color.Yellow;
-            this.button2.Location = new System.Drawing.Point(264, 509);
+            this.button2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(264, 484);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(139, 53);
+            this.button2.Size = new System.Drawing.Size(177, 78);
             this.button2.TabIndex = 2;
             this.button2.Text = "Trekant";
             this.button2.UseVisualStyleBackColor = false;
@@ -62,9 +64,10 @@
             // button3
             // 
             this.button3.BackColor = System.Drawing.Color.RoyalBlue;
-            this.button3.Location = new System.Drawing.Point(73, 586);
+            this.button3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(35, 586);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(139, 53);
+            this.button3.Size = new System.Drawing.Size(177, 78);
             this.button3.TabIndex = 3;
             this.button3.Text = "Kvadrat";
             this.button3.UseVisualStyleBackColor = false;
@@ -73,9 +76,10 @@
             // button4
             // 
             this.button4.BackColor = System.Drawing.Color.MediumOrchid;
+            this.button4.Font = new System.Drawing.Font("Arial Rounded MT Bold", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button4.Location = new System.Drawing.Point(264, 586);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(139, 53);
+            this.button4.Size = new System.Drawing.Size(177, 78);
             this.button4.TabIndex = 4;
             this.button4.Text = "Rektangel";
             this.button4.UseVisualStyleBackColor = false;
